d9138bdb7b3318d443828203d52553877df3ff25
AshrafLobo <ashraflobo@gmail.com>
2020-12-03 14:55:00 +0300

    Add ability to choose button size

--- src/app/components/custom-button/custom-button.component.ts
+++ src/app/components/custom-button/custom-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-custom-button',
@@ -6,5 +6,7 @@ import { Component, EventEmitter, Output } from '@angular/core';
   styleUrls: ['./custom-button.component.scss'],
 })
 export class CustomButtonComponent {
+  @Input('large') large: boolean;
+
   constructor() {}
 }

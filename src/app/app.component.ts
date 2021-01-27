a231a09f2fbe5cea7dacfa34cab7a5aa3764a615
AshrafLobo <ashraflobo@gmail.com>
2021-01-27 13:41:14 +0300

    Shareholder tab in share registration page is active when navigating from shareholders page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,34 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  currentRoute: string;
+  previousRoute: string;
+
+  routesObj = {};
+
+  constructor(private router: Router) {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event) => {
+        const obj = sessionStorage.getItem('routesObj');
+
+        if (!obj) {
+          this.routesObj['currentRoute'] = event;
+          this.routesObj['previousRoute'] = '';
+          sessionStorage.setItem('routesObj', JSON.stringify(this.routesObj));
+        } else {
+          const parsedObj = JSON.parse(obj);
+          this.routesObj['previousRoute'] = parsedObj['currentRoute'];
+          this.routesObj['currentRoute'] = event;
+          sessionStorage.setItem('routesObj', JSON.stringify(this.routesObj));
+        }
+      });
+  }
+}
--- src/app/pages/share-registration/share-registration.component.ts
+++ src/app/pages/share-registration/share-registration.component.ts
@@ -29,7 +29,17 @@ export class ShareRegistrationComponent implements OnInit {
     },
   ];
 
+  selectedIndex: number;
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    const routesObj = JSON.parse(sessionStorage.getItem('routesObj'));
+
+    if (routesObj.previousRoute.url == '/issuers') {
+      this.selectedIndex = 0;
+    } else if (routesObj.previousRoute.url === '/shareholders') {
+      this.selectedIndex = 1;
+    }
+  }
 }

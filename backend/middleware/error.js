162f100f4eea4cc1a3a666a74b4a815386bd1a79
AshrafLobo <ashraflobo@gmail.com>
2021-06-07 16:36:02 +0300

    remove backend

--- backend/middleware/error.js
+++
@@ -1,7 +0,0 @@
-const winston = require('winston');
-
-module.exports = function (err, req, res, next) {
-  console.log("Your error is", err.message);
-  // winston.error(err.message, { metadata: err });
-  res.status(500).send('Something failed.');
-};
--- backend/startup/config.js
+++
@@ -1,8 +0,0 @@
-const config = require('config');
-
-module.exports = function () {
-  /** Check if jwtPrivateKey is set */
-  if (!config.get('jwtPrivateKey')) {
-    throw new Error('FATAL ERROR: jwtPrivateKey is not defined.');
-  }
-}
